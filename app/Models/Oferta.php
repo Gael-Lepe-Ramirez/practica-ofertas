33ce3021c64aaf935cc77737251c622f0f17b190
Gael Lepe Ramírez <gael.lepe3651@alumnos.udg.mx>
2026-03-11 23:46:05 -0600

    Creación de modelo, migración y controlador base para Ofertas

---
+++ app/Models/Oferta.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Oferta extends Model
+{
+    protected $fillable = [
+        'titulo', 
+        'vigencia', 
+        'tienda', 
+        'precio_original', 
+        'precio_descuento'
+    ];
+}
